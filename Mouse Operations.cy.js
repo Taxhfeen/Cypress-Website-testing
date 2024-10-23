29ede224e51ea8936794e63edc7274da64c22ba6
Tashfeen <33594600+Taxhfeen@users.noreply.github.com>
2024-10-23 21:44:15 +0500

    Update Mouse Operations.cy.js

--- Mouse Operations.cy.js
+++ Mouse Operations.cy.js
@@ -1,6 +1,8 @@
-describe (" Mouse Operations",()=>{
+import 'cypress-iframe'
 
-    it.only('MouseHover',()=>{
+describe (" Mouse Operations",()=>{
+   
+    it('MouseHover',()=>{
 
        cy.visit("https://demo.opencart.com/");
 
@@ -13,5 +15,43 @@ describe (" Mouse Operations",()=>{
        .should("be.visible");
     });
 
+    it('Rigt Click',()=>{
+        cy.visit("https://swisnl.github.io/jQuery-contextMenu/demo.html");
+
+        //Approach 1
+       // cy.get(".context-menu-one.btn.btn-neutral").trigger("contextmenu");
+      //  cy.get('.context-menu-icon-copy > span').should('be.visible');
+       
+        //Approach 2
+        cy.get(".context-menu-one.btn.btn-neutral").rightclick();
+        cy.get('.context-menu-icon-copy > span').should('be.visible');
+    });
+
+    it('Double Click',()=>{
+
+       cy.visit("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick3")
+       cy.frameLoaded("#iframeResult")
+
+       // Approach 1 - trigger
+       //cy.iframe("#iframeResult").find("button[ondblclick='myFunction()']").trigger('dblclick')
+      // cy.iframe("#iframeResult").find("#field2").should('have.value','Hello World!')
+
+       // Approach 2 -dbclick 
+       cy.iframe("#iframeResult").find("button[ondblclick='myFunction()']").dblclick()
+       cy.iframe("#iframeResult").find("#field2").should('have.value','Hello World!')
+
+    });
+
+
+    it.only('Scrolling page',()=>{
+        cy.visit("https://www.sic-info.org/en/services/lending/national-flags/")
+
+        cy.get(':nth-child(5) > :nth-child(11)').scrollIntoView({duration:2000})
+        cy.get(':nth-child(5) > :nth-child(11)').should('be.visible')
+
+        cy.get('.contentBody > :nth-child(13) > :nth-child(1)').scrollIntoView({duration:2000})
+        cy.get('.contentBody > :nth-child(13) > :nth-child(1)').should('be.visible')
+
+    });
 
 })
